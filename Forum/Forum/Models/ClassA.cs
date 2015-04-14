06aba83f452803e48ce4dfc45fc66588f434ebd6
milon06cse@gmail.com <milon06cse@gmail.com>
2015-04-14 12:34:56 +0600

    data access project

---
+++ Forum/Forum/Models/ClassA.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Forum.Models
+{
+    public class ClassA:IClassA
+    {
+
+        public int Duration
+        {
+            get;
+            set;
+        }
+
+        public void start()
+        {         
+          // MessageBox.Show("Started");
+        }
+    }
+}
