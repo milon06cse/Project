47d9cfe6cb92accf0e40b76f59049558846f9c47
milon06cse@gmail.com <milon06cse@gmail.com>
2015-04-14 23:40:40 +0600

    Data Access

---
+++ Forum/Forum.DataAccess/BaseRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forum.DataAccess
+{
+    abstract public class BaseRepository
+    {
+        protected DbConnection Connection { get; set; }
+        protected virtual bool OpenConnection()
+        {
+            if (Connection == null)
+                Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ForumDbConnection"].ConnectionString);
+            if (Connection != null && Connection.State != System.Data.ConnectionState.Open)
+            {
+                Connection.Open();
+                return true;
+            }
+            return false;
+        }
+        protected virtual bool CloseConnection()
+        {
+            if (Connection != null && Connection.State == System.Data.ConnectionState.Open)
+            {
+                Connection.Close();
+                return true;
+            }
+            return false;
+        }
+        protected virtual DbCommand CreateSqlCommand(string CommandText)
+        {
+            return new SqlCommand(CommandText,(SqlConnection)Connection);
+        }
+        protected virtual DbParameter CreateParameter(string name,object value)
+        {
+            return new SqlParameter(name, value);
+        }
+
+        protected virtual object ExecuteScalar(String sql)
+        {
+            OpenConnection();
+            object obj = null;
+            DbDataReader dataReader = null;
+            DbCommand command = (DbCommand)new SqlCommand();
+            try
+            {
+                command.CommandText = sql;
+                command.Connection = Connection;
+                //connection.Open();
+            dataReader = command.ExecuteReader();
+
+                if (dataReader.Read())
+                {
+                    obj = (object)dataReader[0];
+                }
+            }
+            catch (DbException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                CloseConnection();
+                dataReader.Close();
+            }
+
+            return obj;
+        }
+
+        public virtual int MaxColumnValue(string Column, String TableName)
+        {
+            string sql = string.Format("select isnull( Max({0})+1,'1') from {1}", Column, TableName);
+            object obj = ExecuteScalar(sql);
+            return Convert.ToInt32(obj);
+        }
+    }
+}
---
+++ Forum/Forum.DataAccess/ForumSectionRepository.cs
@@ -0,0 +1,53 @@
+﻿using Forum.ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forum.DataAccess
+{
+    public class ForumSectionRepository:BaseRepository
+    {
+        public bool Add(ForumSection Section)
+        {
+            try
+            {
+                OpenConnection();
+                DbCommand Command = CreateSqlCommand("insert into ForumSection(SectionId,SectionName,DisplayOrder) values(@SectionId,@SectionName,@DisplayOrder)");
+                Command.Parameters.Add(CreateParameter("SectionId", Section.SectionId));
+                Command.Parameters.Add(CreateParameter("SectionName", Section.SectionName));
+                Command.Parameters.Add(CreateParameter("DisplayOrder", Section.DisplayOrder));
+                Command.ExecuteNonQuery();
+                CloseConnection();
+                return true;
+            }
+            catch
+            {
+                CloseConnection();
+                return false;
+            }
+        }
+        public bool Edit(ForumSection Section)
+        {
+            return false;
+        }
+        public bool Delete(ForumSection Section)
+        {
+            return false;
+        }
+        public ForumSection ShowBySectionId()
+        {
+            ForumSection Section = new ForumSection();
+
+            return Section;
+        }
+        public List<ForumSection> ShowAll()
+        {
+            List<ForumSection> Sections = new List<ForumSection>();
+
+            return Sections;
+        }
+    }
+}
